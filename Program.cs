4771e3419bac3fe7301eb9d3157b201466d98785
GJW <gjw1992@gmail.com>
2018-04-15 15:35:10 +0100

    Object play

--- Program.cs
+++ Program.cs
@@ -8,7 +8,20 @@ namespace ConsoleProject
         {
             var couldBeAnInt = 93;
             Console.WriteLine($"Hello Gary! {couldBeAnInt}");
+            Console.WriteLine($"And show this once more {couldBeAnInt}");
+            Console.WriteLine( (new SomeDataEntity()).Info);
         }
+    }
+
+    class SomeDataEntity
+    {
+        private DateTime creationDateTime;
+        public SomeDataEntity()
+        {
+            this.creationDateTime = DateTime.Now;
+        }
+
+        public string Info => $"{this.GetType().ToString()} Created: {this.creationDateTime}";
     }
 }
 
