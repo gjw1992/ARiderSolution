4980bbcd22420e84fdd376da3d1a625b99e8e624
gjw1992 <gjw1992@gmail.com>
2018-04-22 12:41:05 +0100

    Vostro changes made

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace ConsoleProject
 {
@@ -6,11 +8,12 @@ namespace ConsoleProject
     {
         static void Main(string[] args)
         {
-            var couldBeAnInt = 93;
+            var couldBeAnInt = 94;
             Console.WriteLine($"Hello Gary! {couldBeAnInt}");
             Console.WriteLine($"And show this once more {couldBeAnInt}");
             var oneSomeDataEntity=new SomeDataEntity(23);
-            Console.WriteLine( oneSomeDataEntity.Info);
+            Console.WriteLine(oneSomeDataEntity.Info);
+            Console.WriteLine(oneSomeDataEntity.Version);
         }
     }
 
@@ -23,9 +26,18 @@ namespace ConsoleProject
             this.creationDateTime = DateTime.Now;
         }
 
+        private string buildTime()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            FileInfo fi = new FileInfo(asm.Location);
+            return $"Created: {fi.CreationTime.ToString("yyyy-MM-dd HH:mm:ss")}, Last build: {fi.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")}" ;
+        }
+
         private DateTime creationDateTime;
         public int AnyCount { get; }
         public string Info => $"{this.GetType().ToString()} Created: {this.creationDateTime} AnyCount: {this.AnyCount}";
+
+        public string Version => this.buildTime();
     }
 }
 
