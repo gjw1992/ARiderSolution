879bb09fbfa3c14714f968a234fb20e1d35da4aa
Gary J Walker <gjw1992@gmail.com>
2018-04-22 21:04:47 +0100

    github comment added

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ namespace ConsoleProject
 
         public string Version => this.buildTime();
 
-        private readonly bool monica = true;
+        private readonly bool monica = true; // Added on github
     }
 }
 
